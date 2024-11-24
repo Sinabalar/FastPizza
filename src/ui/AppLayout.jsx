4dd999479663ef3dcc5cd0dcc365eae4075a3e32
Sinabalar <sinabalar666@gmail.com>
2024-11-24 21:14:17 +0330

    feat: add loading state to AppLayout with Loader component
    - Implement loading state handling using useNavigation hook
    - Add conditional rendering to show Loader during route transitions
    - Create basic Loader component structure

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import Header from "./Header.jsx";
 import CartOverview from "../features/cart/CartOverview.jsx";
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigation} from "react-router-dom";
+import Loader from "./Loader.jsx";
 
 export default function AppLayout() {
+
+    const navigation = useNavigation();
+    const isLoading = navigation.state === "loading";
+
     return (
-        <div>
-            <Header/>
-            <main>
-                <h1>Content</h1>
-                <Outlet/>
-            </main>
-            <CartOverview/>
+        <div className={'layout'}>
+            {isLoading
+                ? <Loader/>
+                :
+                <>
+                    <Header/>
+                    <main>
+                        <h1>Content</h1>
+                        <Outlet/>
+                    </main>
+                    <CartOverview/>
+                </>
+            }
         </div>
     );
 }
