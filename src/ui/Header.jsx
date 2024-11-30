2de7c6f59e8a8ac9fa6bd167cbec9d8f37e84794
Sinabalar <sinabalar666@gmail.com>
2024-11-30 19:34:26 +0330

    Enhance Header and Home components with UserName and CreateUser
    - Add UserName component with styled text
    - Integrate UserName into Header component
    - Add CreateUser component to Home page
    - Improve styling and layout of Header and Home components
    - Update import statements and formatting

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import SearchOrder from "../features/order/searchOrder.jsx";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/searchOrder.jsx'
+import UserName from '../features/users/UserName.jsx'
 
 export default function Header() {
-    return (
-        <header className={"bg-yellow-500"}>
-            <Link to={"/"}>FastPizza</Link>
-            <SearchOrder/>
-            <p>Sina</p>
-        </header>
-    );
+  return (
+    <header
+      className={'border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase'}
+    >
+      <Link to={'/'}>FastPizza</Link>
+      <SearchOrder />
+      <UserName />
+    </header>
+  )
 }
