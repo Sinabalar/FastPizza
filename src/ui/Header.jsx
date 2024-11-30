34530b3aaa79e700f45f75573509db79f9bc1480
Sinabalar <sinabalar666@gmail.com>
2024-11-30 18:59:29 +0330

    Implement Tailwind CSS styling for UI components
    - Add Tailwind CSS configuration and import statements
    - Style Header, Home, and CartOverview components
    - Update Prettier configuration for Tailwind CSS
    - Remove old CSS styles and replace with Tailwind utility classes

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import SearchOrder from "../features/order/searchOrder.jsx";
 
 export default function Header() {
     return (
-        <header>
+        <header className={"bg-yellow-500"}>
             <Link to={"/"}>FastPizza</Link>
             <SearchOrder/>
             <p>Sina</p>
