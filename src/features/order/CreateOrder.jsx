7e0fbeab944aff96791348244e0c0144657e1941
Sinabalar <sinabalar666@gmail.com>
2024-12-04 13:50:10 +0330

    style: Update index.css with Tailwind CSS input styles and loader animation
    - Removed commented-out styles for cleaner code.
    - Added a custom input style using Tailwind CSS.
    - Maintained existing loader animation with Tailwind CSS adjustments.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -46,27 +46,13 @@ function CreateOrder() {
       <Form method={"POST"}>
         <div>
           <label>First Name</label>
-          <input
-            type="text"
-            name="customer"
-            required
-            className={
-              "w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-400 md:px-5 md:py-3"
-            }
-          />
+          <input type="text" name="customer" required className={"input"} />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input
-              type="tel"
-              name="phone"
-              required
-              className={
-                "w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-400 md:px-5 md:py-3"
-              }
-            />
+            <input type="tel" name="phone" required className={"input"} />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -74,14 +60,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              type="text"
-              name="address"
-              required
-              className={
-                "w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-400 md:px-5 md:py-3"
-              }
-            />
+            <input type="text" name="address" required className={"input"} />
           </div>
         </div>
 
@@ -90,7 +69,9 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            className={"accent-yellow-400 h-6 w-6 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-offset-2"}
+            className={
+              "h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-offset-2"
+            }
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
