b2bccf3c0ca2015bfced1d14c8da3e81059528fe
Sinabalar <sinabalar666@gmail.com>
2024-12-06 19:09:53 +0330

    style: Refactor components and enhance styling with Tailwind CSS
    - Updated `Cart.jsx` to include `CartItem.jsx` component and improved styling.
    - Enhanced `CartItem.jsx` with Tailwind CSS classes for better layout and styling.
    - Corrected CSS in `Button.jsx` to ensure `bg-transparent` class is applied correctly.
    - Modified `index.html` to update title and icon.
    - Improved form layout and styling in `CreateOrder.jsx` with Tailwind CSS.
    - Adjusted `index.css` to refine input styling.

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom"
 
 export default function Button({ children, disabled, to, type }) {
   const base =
-    "inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-all duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+    "inline-block rounded-full font-semibold uppercase tracking-wide text-stone-800 transition-all duration-300 focus:outline-none focus:ring focus:ring-offset-2 disabled:cursor-not-allowed"
 
   const styles = {
-    primary: base + " px-4 py-3 sm:px-6 sm:py-4",
-    secondary: base + " bg-transparent border-2 border-stone-300 px-4 py-2.5 sm:px-6 sm:py-3.5 text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:ring-stone-200 focus:bg-stone-300 focus:text-stone-800",
-    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    primary:
+      base +
+      " bg-yellow-400 px-4 py-3 sm:px-6 sm:py-4 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring-yellow-300",
+    secondary: `${base} bg-transparent border-2 border-stone-300 px-4 py-2.5 sm:px-6 sm:py-3.5 text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:ring-stone-200 focus:bg-stone-300 focus:text-stone-800`,
+    small:
+      base +
+      " bg-yellow-400 px-4 py-2 md:px-5 md:py-2.5 text-xs hover:bg-yellow-300 focus:bg-yellow-300 focus:ring-yellow-300",
   }
 
   if (to) {
