3aea9cb9278a668c808791415fe50ffe49e239c8
Sinabalar <sinabalar666@gmail.com>
2024-12-07 16:02:32 +0330

    feat: Add cart slice to Redux store
    - Created `cartSlice.js` to manage cart state with actions for adding, deleting, and updating items.
    - Integrated `cartReducer` into the Redux store in `store.js`.

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userReducer from "./features/users/userSlice.js"
+import cartReducer from "./features/cart/cartSilce.js"
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cart:cartReducer,
   },
 })
 
